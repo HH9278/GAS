cd7f3a8b90834ce595f90cbdc3c35c8afad94440
HH9278 <67408627+HH9278@users.noreply.github.com>
2026-04-26 05:46:38 +0900

    Create deleteFromSpamFolderImmediately.js

---
+++ deleteFromSpamFolderImmediately.js
@@ -0,0 +1,26 @@
+function deleteFromSpamFolderImmediately() {
+  // ここに即削除したい送信元メールアドレスを指定
+  const targetEmails = [
+    "t-saeki@r-agent.com",
+    "noreply@lancers.co.jp",
+    "message@salon-ms.jp"
+  ];
+
+  // スパムフォルダ内の直近7日分を検索
+  const threads = GmailApp.search("in:spam newer_than:7d");
+
+  for (const thread of threads) {
+    const messages = thread.getMessages();
+
+    // スレッド内に対象メアドからのメールが1件でもあれば削除
+    const shouldDelete = messages.some(msg => {
+      const from = msg.getFrom(); 
+      return targetEmails.some(email => from.includes(email));
+    });
+
+    if (shouldDelete) {
+      thread.moveToTrash();
+      Logger.log(`Moved to trash: ${thread.getFirstMessageSubject()}`);
+    }
+  }
+}
